3f4e82575c44aaa154c3a6ac4fe3d872837b8919
wnstj1548 <wnstj0614@naver.com>
2024-12-04 19:33:36 +0900

    feat : "홈 화면 관련 repository 작성 완료"

--- src/main/java/com/mayo/client/mayoclientapi/application/service/ItemService.java
+++ src/main/java/com/mayo/client/mayoclientapi/application/service/ItemService.java
@@ -1,7 +1,6 @@
 package com.mayo.client.mayoclientapi.application.service;
 
 import com.mayo.client.mayoclientapi.persistance.repository.ItemRepository;
-import com.mayo.client.mayoclientapi.presentation.dto.response.ReadItemResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
