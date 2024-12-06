e7615106c654dcd191718e40b4467c48b18b3e40
wnstj1548 <wnstj0614@naver.com>
2024-12-06 16:11:55 +0900

    feat : "FAQ Type Integer로 변경"

--- src/main/java/com/mayo/client/mayoclientapi/persistance/domain/type/FAQType.java
+++ src/main/java/com/mayo/client/mayoclientapi/persistance/domain/type/FAQType.java
@@ -2,21 +2,31 @@ package com.mayo.client.mayoclientapi.persistance.domain.type;
 
 public enum FAQType {
 
-    UPDATE_INFO("정보 수정"),
-    DELETE_ACCOUNT("회원 탈퇴"),
-    STORE_PICKUP("매장/픽업"),
-    ORDER_HISTORY("주문 내역"),
-    ORDER_NOTIFICATION("주문 알림"),
-    MAIN_FEATURES("앱 주요 기능"),
-    HOW_TO_USE("앱 이용 방법"),
-    PAYMENT("결제"),
-    COUPON("쿠폰"),
-    EVENT("이벤트"),
-    ETC("기타");
+    UPDATE_INFO(11, "정보 수정"),
+    DELETE_ACCOUNT(12, "회원 탈퇴"),
+    STORE_PICKUP(13, "매장/픽업"),
+    ORDER_HISTORY(14, "주문 내역"),
+    ORDER_NOTIFICATION(15, "주문 알림"),
+    MAIN_FEATURES(16, "앱 주요 기능"),
+    HOW_TO_USE(17, "앱 이용 방법"),
+    PAYMENT(18, "결제"),
+    COUPON(19, "쿠폰"),
+    EVENT(20, "이벤트"),
+    ETC(21, "기타");
 
+    private final int code;
     private final String text;
 
-    FAQType(String text) {
+    FAQType(int code, String text) {
+        this.code = code;
         this.text = text;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getText() {
+        return text;
     }
 }
