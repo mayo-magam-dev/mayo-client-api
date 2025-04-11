42c7a31bb57eef3fe932c9d7e1bb64f03cde90f7
wnstj1548 <wnstj0614@naver.com>
2025-04-11 21:56:55 +0900

    feat : "datetime 형식 timestamp로 변경 추가"

--- src/main/java/com/mayo/client/mayoclientapi/common/serializer/TimestampDeserializer.java
+++ src/main/java/com/mayo/client/mayoclientapi/common/serializer/TimestampDeserializer.java
@@ -12,6 +12,11 @@ public class TimestampDeserializer extends JsonDeserializer<Timestamp> {
     @Override
     public Timestamp deserialize(JsonParser parser, DeserializationContext context) throws IOException {
         String date = parser.getText();
+
+        if (date.contains(" ") && date.endsWith("Z")) {
+            date = date.replace(" ", "T");
+        }
+
         return Timestamp.parseTimestamp(date);
     }
 }
