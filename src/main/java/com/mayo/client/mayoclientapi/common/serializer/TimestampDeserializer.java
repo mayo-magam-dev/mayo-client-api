ceeb53970bf602a4a6a96b4dd24499b2f3927dd2
wnstj1548 <wnstj0614@naver.com>
2025-04-11 21:13:18 +0900

    fix : "serializer 적용"

---
+++ src/main/java/com/mayo/client/mayoclientapi/common/serializer/TimestampDeserializer.java
@@ -0,0 +1,17 @@
+package com.mayo.client.mayoclientapi.common.serializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.google.cloud.Timestamp;
+
+import java.io.IOException;
+
+public class TimestampDeserializer extends JsonDeserializer<Timestamp> {
+
+    @Override
+    public Timestamp deserialize(JsonParser parser, DeserializationContext context) throws IOException {
+        String date = parser.getText();
+        return Timestamp.parseTimestamp(date);
+    }
+}
